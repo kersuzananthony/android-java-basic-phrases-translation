b1eefac7a37a76a4c3e47e1d1a94e91fe85de971
Kersuzan Anthony <kersuzan.anthony@gmail.com>
2016-03-14 10:16:14 +0800

    Adds basic phrases m4a files

---
+++ app/src/main/java/com/kersuzan/basicphrases/MainActivity.java
@@ -0,0 +1,57 @@
+package com.kersuzan.basicphrases;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    public void buttonTapped(View view) {
+        int viewId = view.getId(); // Give an integer (not the string we specified)
+        String ourTextId = "";
+
+
+        ourTextId = view.getResources().getResourceEntryName(viewId); // Get the string associated with the int id
+        int resourceId = getResources().getIdentifier(ourTextId, "raw", getPackageName()); // Get the audio file resource id
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceId);
+        mediaPlayer.start();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
